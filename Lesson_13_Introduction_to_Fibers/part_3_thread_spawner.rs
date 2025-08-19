144e71044c090d7c4ff7f7c2a144c919de8bea8a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-19 14:26:42 -0400

    Add Lesson_13_Introduction_to_Fibers/part_3_thread_spawner.rs

---
+++ Lesson_13_Introduction_to_Fibers/part_3_thread_spawner.rs
@@ -0,0 +1,19 @@
+pub fn spawn(&mut self, f: fn()) {
+    let available = self
+        .threads
+        .iter_mut()
+        .find(|t| t.state == State::Available)
+        .expect("No available threads.");
+
+    let size = available.stack.len();
+    unsafe {
+        let s_ptr = available.stack.as_mut_ptr().offset(size as isize);
+        let s_ptr = (s_ptr as usize & !15) as *mut u8; // Align to 16 bytes
+        std::ptr::write(s_ptr.offset(-16) as *mut u64, guard as u64);
+        std::ptr::write(s_ptr.offset(-24) as *mut u64, skip as u64);
+        std::ptr::write(s_ptr.offset(-32) as *mut u64, f as u64);
+        available.ctx.rsp = s_ptr.offset(-32) as u64;
+    }
+
+    available.state = State::Ready;
+}
