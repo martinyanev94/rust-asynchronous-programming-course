3dabdb858f27a67df59d05883a3f2a5fdf39385b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-19 14:28:59 -0400

    Add Lesson_5_Handling_Asynchronous_Operations/part_1_asyncTasks.js

---
+++ Lesson_5_Handling_Asynchronous_Operations/part_1_asyncTasks.js
@@ -0,0 +1,7 @@
+console.log("Task 1: Start");
+
+setTimeout(() => {
+    console.log("Task 2: Delayed Task Completed");
+}, 2000); // 2 seconds delay
+
+console.log("Task 3: Continue with Other Work");
